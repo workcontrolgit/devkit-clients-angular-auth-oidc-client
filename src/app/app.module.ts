5a3622c13aaf728adb3b4599ff232a56f34b35d2
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-09-18 00:03:39 -0400

    init version

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AuthModule, OidcConfigService } from 'angular-auth-oidc-client';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig({
+      stsServer: 'https://devkit-sts.azurewebsites.net',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'devkit-clients-spa.pkce',
+      scope: 'openid profile email roles app.api.employeeprofile.read',
+      responseType: 'code',
+      silentRenew: true,
+      silentRenewUrl: '${window.location.origin}/silent-renew.html',
+      useRefreshToken: false,
+    });
+}
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, UnauthorizedComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'unauthorized', component: UnauthorizedComponent },
+    ]),
+    AuthModule.forRoot(),
+    HttpClientModule,
+  ],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
